5dfdcdf4c03b9905ad7cf2056a65a06367384e2e
Milcah <milcah@macbook-air.lan>
2020-12-23 15:42:17 -0800

    add button event listeners and decrement metric functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /** 
 Specifications:
 Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
-Add the ability to name your pet.
 Style the page.
 Increase your pet's age every x minutes
 Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
@@ -39,24 +38,49 @@ const tamagotchi = new Pet(petName);
 // variables
 
 /** cached DOM elements */
-const playBtn = document.getElementById('play-btn')
+const startBtn = document.getElementById('start-btn')
+const instructBtn = document.getElementById('instruct-btn')
 const petNameElems = document.querySelectorAll('.pet-name')
 const age = document.getElementById('age')
 const hunger = document.getElementById('hunger')
 const sleepiness = document.getElementById('sleepiness')
 const boredom = document.getElementById('boredom')
-
-/* event listeners */
+const eatBtn = document.getElementById('eat-btn')
+const sleepBtn = document.getElementById('sleep-btn')
+const playBtn = document.getElementById('play-btn')
 
 /* functions */
-function startGame() {
+const displayPetName = () => {
   for (let i = 0; i < petNameElems.length; i++) {
     petNameElems[i].textContent = tamagotchi.name
   }
+}
+
+const displayPetMetrics = () => {
   age.textContent = tamagotchi.age
   hunger.textContent = tamagotchi.hunger
   sleepiness.textContent = tamagotchi.sleepiness
   boredom.textContent = tamagotchi.boredom
 }
 
+const decrementPetMetric = (metric) => {
+  if (tamagotchi[`${metric}`] === 0) {
+    return
+  }
+    tamagotchi[metric]--
+    console.log(tamagotchi[`${metric}`])
+}
+
+const startGame = () => {
+  displayPetName()
+  displayPetMetrics()
+}
+
 startGame()
+
+/* event listeners */
+startBtn.addEventListener('click', () => console.log('play clicked'))
+instructBtn.addEventListener('click', () => console.log('instructions clicked'))
+eatBtn.addEventListener('click', () => decrementPetMetric('hunger'))
+sleepBtn.addEventListener('click', () => decrementPetMetric('sleepiness'))
+playBtn.addEventListener('click', () => decrementPetMetric('boredom'))
